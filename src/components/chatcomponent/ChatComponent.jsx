475b91927fae6c987ecd946fefa9a7d517eee9f2
dhanush580 <pkveeragautham10@gmail.com>
2025-05-27 15:23:58 +0530

    css changes

--- src/components/chatcomponent/ChatComponent.jsx
+++ src/components/chatcomponent/ChatComponent.jsx
@@ -182,9 +182,9 @@ export default function DeepSeekChat() {
           ) : (
             <div className="empty-state">
               <div className="magic-wand-icon">
-                <svg viewBox="0 0 24 24">
-                  <path d="M20 7h-4V5l-2-2h-4L8 5v2H4c-1.1 0-2 .9-2 2v5c0 .75.4 1.38 1 1.73V19c0 1.11.89 2 2 2h14c1.11 0 2-.89 2-2v-3.28c.59-.35 1-.99 1-1.72V9c0-1.1-.9-2-2-2zM10 5h4v2h-4V5zM4 9h16v5h-5v-3H9v3H4V9zm9 6h-2v-2h2v2zm6 4H5v-3h4v1h6v-1h4v3z"/>
-                </svg>
+                <span class="material-symbols-outlined robo-icon">
+                  smart_toy
+                </span>
               </div>
               <h3>Create Your First Story</h3>
               <p>Enter a prompt below to generate a magical story</p>
@@ -216,10 +216,10 @@ export default function DeepSeekChat() {
               <span className="spinner"></span>
             ) : (
               <>
-                <span>Weave Magic</span>
-                <svg className="magic-icon" viewBox="0 0 24 24">
-                  <path d="M20 7h-4V5l-2-2h-4L8 5v2H4c-1.1 0-2 .9-2 2v5c0 .75.4 1.38 1 1.73V19c0 1.11.89 2 2 2h14c1.11 0 2-.89 2-2v-3.28c.59-.35 1-.99 1-1.72V9c0-1.1-.9-2-2-2zM10 5h4v2h-4V5zM4 9h16v5h-5v-3H9v3H4V9zm9 6h-2v-2h2v2zm6 4H5v-3h4v1h6v-1h4v3z"/>
-                </svg>
+                <span>Generate</span>
+                <span class="material-symbols-outlined">
+                   send
+                </span>
               </>
             )}
           </button>
